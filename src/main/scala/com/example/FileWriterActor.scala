b848e26f7744035f748b488394d62a882fc64056
Divan Visagie <visagiedivan@gmail.com>
2016-03-26 12:17:06 +0200

    init

---
+++ src/main/scala/com/example/FileWriterActor.scala
@@ -0,0 +1,24 @@
+package com.example
+
+import java.nio.file.{Files, Paths}
+
+import akka.actor.{PoisonPill, Props, Actor}
+import akka.event.Logging
+
+class FileWriterActor extends Actor {
+  val log = Logging.getLogger(context.system, this)
+
+  def receive = {
+    case words: List[x] => {
+
+      println(s"doing fake write ${words}")
+
+      sender() ! DoneWriting()
+      self ! PoisonPill
+    }
+  }
+}
+
+object FileWriterActor {
+  def props = Props(new FileWriterActor)
+}
